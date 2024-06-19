f6bd5391641be4722b94202675a2c79eea9a2df0
Vishal Kalita <vishalkalita036@gmail.com>
2024-06-20 00:42:52 +0530

    form validation added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,30 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
 
   const ToggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
+  };
+
+  const handleButtonClick = () => {
+    //validate form data
+    // console.log(email.current.value);
+    // console.log( password.current.value);
+    const message = checkValidData(
+      email.current.value,
+      password.current.value,
+      name.current.value
+    );
+    // console.log(message);
+    setErrorMessage(message);
   };
 
   return (
@@ -13,33 +32,44 @@ const Login = () => {
       <Header />
       <div className="absolute">
         <img
+          className="bg-black"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/a56dc29b-a0ec-4f6f-85fb-50df0680f80f/2f8ae902-8efe-49bb-9a91-51b6fcc8bf46/IN-en-20240617-popsignuptwoweeks-perspective_alpha_website_small.jpg"
           alt="background"
         />
       </div>
-      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-75">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-75"
+      >
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
         {!isSignInForm && (
           <input
+            ref={name}
             type="text"
             placeholder="Full Name"
-            className="px-4 py-3 my-4 w-full bg-gray-800 rounded-lg text-sm"
+            className="px-4 py-3 my-4 w-full bg-gray-800 bg-opacity-70 rounded-lg text-sm"
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email Address"
-          className="px-4 py-3 my-4 w-full bg-gray-800 rounded-lg text-sm"
+          className="px-4 py-3 my-4 w-full bg-gray-800 bg-opacity-70 rounded-lg text-sm"
         />
 
         <input
+          ref={password}
           type="password"
           placeholder="Password"
-          className="px-4 py-3 my-4 w-full bg-gray-800 rounded-lg text-sm"
+          className="px-4 py-3 my-4 w-full bg-gray-800 bg-opacity-70 rounded-lg text-sm"
         />
-        <button className="px-4 py-2 my-6 bg-red-700 w-full rounded-lg  ">
+        <p className="text-red-500 text-xs font-medium">{errorMessage}</p>
+        <button
+          className="px-4 py-2 my-6 bg-red-700 w-full rounded-lg"
+          onClick={handleButtonClick}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-4 text-sm cursor-pointer" onClick={ToggleSignInForm}>
