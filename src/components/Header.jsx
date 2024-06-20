be4c6a7aa0d950607326c811661305ad2f448e7f
Vishal Kalita <vishalkalita036@gmail.com>
2024-06-21 01:11:27 +0530

    MultiLanguage support added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,15 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { addUser, removeUser } from "../utils/userSlice";
-import { LOGO } from "../utils/constants";
+import { LOGO, SUPPORTED_LANGUAGES } from "../utils/constants";
+import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
+
   const dispatch = useDispatch();
 
   const handleSignOut = () => {
@@ -41,16 +45,42 @@ const Header = () => {
     return () => unsubscribe();
   }, []);
 
+  const handleGptSearchClick = () => {
+    dispatch(toggleGptSearchView());
+  };
+
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
+
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between">
-      <img
-        className="w-44 mx-auto md:mx-0"
-        src={LOGO}
-        alt="logo"
-      />
+      <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
       {user && (
         <div className="flex p-2">
-          <img className="w-12 h-12 rounded-xl" src={user?.photoURL} alt="usericon" />
+          {showGptSearch && (
+            <select
+              className="p-2 bg-gray-900 text-white m-2"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+          <button
+            className="py-2 px-4 mx-4 my-2 bg-red-800 text-white rounded-lg"
+            onClick={handleGptSearchClick}
+          >
+            {showGptSearch ? "HomePage" : "GPT Search"}
+          </button>
+          <img
+            className="w-12 h-12 rounded-xl"
+            src={user?.photoURL}
+            alt="usericon"
+          />
           <button className="font-bold text-white" onClick={handleSignOut}>
             Sign Out
           </button>
